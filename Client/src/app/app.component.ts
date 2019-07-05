f8f8a699b17f7abb670c98e5dfbb79c5ad45cffe
ChrisMeyer7088 <chrismeyer175@gmail.com>
2019-07-05 12:19:45 -0700

    Fixed Footer and Header problem

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-ChrisMeyerSite';
+  clientHeight: number;
+  constructor(){
+    this.clientHeight = window.innerHeight;
+  }
 }
