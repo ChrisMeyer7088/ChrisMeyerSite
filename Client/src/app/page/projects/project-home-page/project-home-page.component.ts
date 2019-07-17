b78995099dae83049dc0a9c5cd4a16ea81b52462
ChrisMeyer7088 <chrismeyer175@gmail.com>
2019-07-17 15:53:45 -0700

    Added mobile compatibility for projects-home page

--- Client/src/app/page/projects/project-home-page/project-home-page.component.ts
+++ Client/src/app/page/projects/project-home-page/project-home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-project-home-page',
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectHomePageComponent implements OnInit {
 
+  public pointMapGit: HTMLElement;
+  public cmSiteGit: HTMLElement;
+  public giftMeGit: HTMLElement;
+  public pointmapTitle: HTMLElement;
+  public cmSiteTitle: HTMLElement;
+  public giftMeTitle: HTMLElement;
+  public innerWidth: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.pointMapGit = document.getElementById('gitlink-pointmap');
+    this.cmSiteGit = document.getElementById('gitlink-cmsite');
+    this.giftMeGit = document.getElementById('gitlink-giftme');
+    this.pointmapTitle = document.getElementById('title-pointmap');
+    this.cmSiteTitle = document.getElementById('title-cmsite');
+    this.giftMeTitle = document.getElementById('title-giftme');
+
+    this.innerWidth = window.innerWidth;
+    this.adjustPageLayout();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onresize(event){
+    this.innerWidth = window.innerWidth;
+    this.adjustPageLayout();
+  }
+
+  adjustPageLayout(){
+    if(this.innerWidth < 990){
+      this.removeLinks();
+    }
+    else if(this.innerWidth < 1560){
+      this.addGitLinks();
+    }
+    else{
+      this.addGitLinks();
+    }
+  }
+
+  addGitLinks(){
+    document.getElementById('container-pointmap').append(this.pointMapGit);
+    document.getElementById('container-cmsite').append(this.cmSiteGit);
+    document.getElementById('container-giftme').append(this.giftMeGit);
+  }
+
+  removeLinks(){
+    this.pointMapGit.remove();
+    this.cmSiteGit.remove();
+    this.giftMeGit.remove();
+  }
 }
