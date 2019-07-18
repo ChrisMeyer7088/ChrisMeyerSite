cf8ab67e236f9253e9223bb89b0a1bfc83fd7609
ChrisMeyer7088 <chrismeyer175@gmail.com>
2019-07-17 17:19:13 -0700

    Added mobile compatibility for projects-giftme page

--- Client/src/app/page/projects/giftme-page/giftme-page.component.ts
+++ Client/src/app/page/projects/giftme-page/giftme-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-giftme-page',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giftme-page.component.scss']
 })
 export class GiftmePageComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
