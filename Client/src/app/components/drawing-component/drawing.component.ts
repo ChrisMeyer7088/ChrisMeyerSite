2516ef8d0ee54f16300eb2ed87cc56f3b4e31074
ChrisMeyer7088 <chrismeyer175@gmail.com>
2019-08-19 10:49:50 -0700

    Initalized fun-page

---
+++ Client/src/app/components/drawing-component/drawing.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'drawing-game',
+  templateUrl: './drawing.component.html',
+  styleUrls: ['drawing.component.scss']
+})
+
+export class DrawingGame implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ Client/src/app/page/fun-page/fun-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fun-page',
+  templateUrl: './fun-page.component.html',
+  styleUrls: ['./fun-page.component.scss']
+})
+
+export class FunPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(){
+
+  }
+}
